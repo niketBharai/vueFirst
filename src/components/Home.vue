8d8773f6306b795b2c458622fd60c6f9dca6db72
Niket <niksbharai.1@gmail.com>
2021-11-03 13:59:59 +0530

    upto two way binding

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Hello {{ username }}</h1>
+  <input type="text" placeholder="Enter Email"/>
+  <br />
+  <br />
+  <input type="text" v-model="count" placeholder="Enter username"/>
+  <br />
+  <br />
+  <button v-on:click="handleClick()">Get Values</button>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      username: "Niket Bharai",
+      count: 0,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.count = this.count + 1;
+    },
+  },
+};
+</script> 
