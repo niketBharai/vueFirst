cfa31259ed811800a3b73757fa2077753e3b0542
Niket <niksbharai.1@gmail.com>
2021-11-03 18:04:16 +0530

    data binding components

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
 <img alt="Vue logo" v-if="show" src="../assets/logo.png" />
-<img alt="React logo" v-else src="../assets/react.png" width="280"/>
+<img alt="React logo" v-else src="../assets/react.png" width="280" />
 <h1>Hello {{ username }}</h1>
 <input type="text" placeholder="Enter Email" v-model="email" />
 <br />
@@ -12,24 +12,60 @@
 <br />
 <br />
 <button v-on:click="show=!show">Change Image</button>
+<ul>
+    <li v-for="item in frameworks" :key="item">
+        {{item}}
+    </li>
+</ul>
 
+<ul>
+    <li v-for="item in users" :key="item">
+        <Users :data="item" :getUser="getUserInfo"/>
+    </li>
+</ul>
 </template>
 
 <script>
+import Users from './User.vue';
+
 export default {
     name: "Home",
+    components: {
+        Users
+    },
     data() {
         return {
             username: "Niket Bharai",
             email: null,
             mobile: null,
-            show: true
+            show: true,
+            frameworks: ["Vue", "React", "Angular"],
+            users: [{
+                    name: "Darlene",
+                    email: "darlenealderson@kmail.com"
+                },
+                {
+                    name: "Elliot",
+                    email: "elliotalderson@kmail.com"
+                },
+                {
+                    name: "Walter",
+                    email: "walter@kmail.com"
+                },
+                {
+                    name: "Edo",
+                    email: "edovoss@dmail.com"
+                }
+            ]
         };
     },
     methods: {
         handleClick() {
             console.log("Values :", this.email, this.mobile)
         },
+        getUserInfo(info) {
+            alert(info)
+        }
     },
 };
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,20 @@
+<template>
+<h3>{{data.name}}</h3>
+<h4>{{data.email}}</h4>
+<button v-on:click="getUser(info)">Send Info</button>
+</template>
+
+<script>
+export default {
+    name: "Users",
+    props: {
+        data: Object,
+        getUser: Function,
+    },
+    data() {
+        return {
+            info: "the info of " + this.data.name
+        }
+    }
+}
+</script>
