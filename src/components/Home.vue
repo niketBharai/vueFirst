32b150d1b0a7168697de75eb403c414737fe86bc
Niket <niksbharai.1@gmail.com>
2021-11-03 14:28:34 +0530

    upto conditional rendering

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,27 +1,35 @@
 <template>
-  <h1>Hello {{ username }}</h1>
-  <input type="text" placeholder="Enter Email"/>
-  <br />
-  <br />
-  <input type="text" v-model="count" placeholder="Enter username"/>
-  <br />
-  <br />
-  <button v-on:click="handleClick()">Get Values</button>
+<img alt="Vue logo" v-if="show" src="../assets/logo.png" />
+<img alt="React logo" v-else src="../assets/react.png" width="280"/>
+<h1>Hello {{ username }}</h1>
+<input type="text" placeholder="Enter Email" v-model="email" />
+<br />
+<br />
+<input type="text" v-model="mobile" placeholder="Enter Mobile Number" />
+<br />
+<br />
+<button v-on:click="handleClick()">Get Values</button>
+<br />
+<br />
+<button v-on:click="show=!show">Change Image</button>
+
 </template>
 
 <script>
 export default {
-  name: "Home",
-  data() {
-    return {
-      username: "Niket Bharai",
-      count: 0,
-    };
-  },
-  methods: {
-    handleClick() {
-      this.count = this.count + 1;
+    name: "Home",
+    data() {
+        return {
+            username: "Niket Bharai",
+            email: null,
+            mobile: null,
+            show: true
+        };
     },
-  },
+    methods: {
+        handleClick() {
+            console.log("Values :", this.email, this.mobile)
+        },
+    },
 };
-</script> 
+</script>
